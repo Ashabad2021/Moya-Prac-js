2f28edd1b888c874e2e0220898ad8d0331f37ad8
Ashabad2021 <aleksey.miheev79@gmail.com>
2022-01-14 16:06:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const div1 = document.querySelector('.div1');
+const div2 = document.querySelector('.div2');
+
+
+const text =`<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur rem totam autem consequuntur nam quibusdam modi, iusto ratione. Sunt, nesciunt.</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nihil?</p>`
+
+
+
+div1.insertAdjacentHTML('beforebegin',text);
+
+div1.insertAdjacentText('beforeend','1111111111111')
+const audio = new Audio
+console.log(audio);
